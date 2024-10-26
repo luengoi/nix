221b991a2b6699bbc7c51572301c16399a48aee9
Iván Luengo <28649633+luengoi@users.noreply.github.com>
2024-10-26 19:16:18 +0200

    manage firefox configuraiton settings

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -5,6 +5,25 @@
     enable = true;
     package = pkgs.firefox-bin;
 
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DontCheckDefaultBrowser = true;
+      DisablePocket = true;
+      SearchBar = "unified";
+
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+    };
+
     profiles.default = {
       id = 0;
 
@@ -31,11 +50,45 @@
         };
       };
 
+      settings = {
+        "signon.rememberSignons" = false;
+        "browser.compactmode.show" = true;
+        "browser.cache.disk.enable" = false;
+        "places.history.enabled" = false;
+        "privacy.history.custom" = true;
+        "privacy.sanitize.sanitizeOnShutdown" = true;
+        "privacy.sanitize.clearOnShutdown.hasMigratedToNewPrefs2" = true;
+        "extensions.pocket.enabled" = false;
+        "browser.topsites.contile.enabled" = false;
+        "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.feeds.topsites" = false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includeVisited" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.activity-stream.newtabWallpapers.wallpaper" = "dark-color";
+        "browser.newtabpage.activity-stream.newtabWallpapers.wallpaper-dark" = "dark-color";
+        "browser.newtabpage.activity-stream.newtabWallpapers.wallpaper-light" = "dark-color";
+        "browser.formfill.enable" = false;
+        "browser.search.suggest.enabled" = false;
+        "browser.search.suggest.enabled.private" = false;
+        "browser.urlbar.suggest.searches" = false;
+        "browser.urlbar.showSearchSuggestionsFirst" = false;
+        "app.shield.optoutstudies.enabled" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
+      };
+
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
         ublock-origin
         darkreader
         multi-account-containers
         foxyproxy-standard
+        privacy-badger
       ];
     };
   };
